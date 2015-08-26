8f86a7d246096167f13b155bfc880d8b2cf7eaff
Alexey Baranov <baranov@digitalcore.ru>
2015-08-26 10:45:14 +0300

    / -> /secret_admin_zone/

--- main/urls.py
+++ main/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from . import views
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = [
+    url('^$', RedirectView.as_view(url='/secret_admin_zone', permanent=False)),
     url(r'^secret_admin_zone/', include(admin.site.urls)),
     # url(r'^acc/', include('acc.urls')),
     url(r'^userlayers/', include('userlayers.urls')),
