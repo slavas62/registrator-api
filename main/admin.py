8260a60ac96f4f01126ae0aedb5815057ba80786
Alexey Baranov <baranov@digitalcore.ru>
2015-09-07 12:36:03 +0300

    UPD geometry

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,16 @@
 # coding: utf-8
 from copy import deepcopy
-import mutant
+from django import forms
 from django.contrib import admin
-from django import forms as django_forms
-from userlayers.api.forms import FIELD_TYPES, GEOMETRY_FIELD_TYPES
+from mutant.models import FieldDefinition
+from userlayers import DEFAULT_MD_GEOMETRY_FIELD_TYPE, DEFAULT_MD_GEOMETRY_FIELD_NAME
 from userlayers.models import ModelDefinition
+from userlayers.api.forms import FIELD_TYPES, GEOMETRY_FIELD_TYPES
+from polymodels.models import PolymorphicModel
 
 
 class FieldDefinitionInlineAdmin(admin.TabularInline):
-    model = mutant.models.FieldDefinition
+    model = FieldDefinition
     fk_name = 'model_def'
     suit_classes = 'suit-tab suit-tab-fields'
 
@@ -25,36 +27,56 @@ class FieldDefinitionInlineAdmin(admin.TabularInline):
         return False
 
 
-class ModelDefinitionFormAdmin(django_forms.ModelForm):
-    # geometry_field = django_forms.CharField(
-    #     widget=django_forms.Select(
-    #         choices=[(_, _) for _ in dict(GEOMETRY_FIELD_TYPES).keys()]), label=u'Геометрия по умолчанию')
+class ModelDefinitionFormAdmin(forms.ModelForm):
+    GEOMETRY_FIELD_CHOICES = [(n, c._meta.verbose_name) for n, c in dict(GEOMETRY_FIELD_TYPES).items()]
+    GEOMETRY_FIELD_LABEL = u'Геометрия по умолчанию'
+    geometry_field = forms.ChoiceField(choices=GEOMETRY_FIELD_CHOICES, label=GEOMETRY_FIELD_LABEL)
 
     class Meta:
         model = ModelDefinition
         widgets = {
-            'verbose_name': django_forms.TextInput(),
-            'verbose_name_plural': django_forms.TextInput(),
+            'verbose_name': forms.TextInput(),
+            'verbose_name_plural': forms.TextInput(),
         }
+
+    def __init__(self, instance=None, *args, **kwargs):
+        super(ModelDefinitionFormAdmin, self).__init__(instance=instance, *args, **kwargs)
+        if instance:
+            geomentry_field = FieldDefinition.objects.get_by_natural_key(
+                instance.app_label, instance.object_name, DEFAULT_MD_GEOMETRY_FIELD_NAME)
+            gt = dict(GEOMETRY_FIELD_TYPES)
+            self.fields['geometry_field'] = forms.ChoiceField(
+                initial=gt.keys()[gt.values().index(geomentry_field.__class__)],
+                choices=self.GEOMETRY_FIELD_CHOICES, label=self.GEOMETRY_FIELD_LABEL)
+            self.fields['geometry_field'].widget.attrs["disabled"] = True
 
 
 class ModelDefinitionAdmin(admin.ModelAdmin):
     model = ModelDefinition
     form = ModelDefinitionFormAdmin
+
     inlines = [FieldDefinitionInlineAdmin]
     fieldsets = [[
         None,
         {
             'classes': ['suit-tab', 'suit-tab-general'],
-            'fields': ['name', 'owner',
-                       # 'geometry_field',
-                       'verbose_name', 'verbose_name_plural']
+            'fields': ['name', 'owner', 'geometry_field', 'verbose_name', 'verbose_name_plural']
         }
     ]]
     suit_form_tabs = [
         ['general', u'Тип'],
         ['fields', u'Поля'],
     ]
+
+    def save_model(self, request, obj, form, change):
+        super(ModelDefinitionAdmin, self).save_model(request, obj, form, change)
+        if not change:
+            geometry_model = dict(GEOMETRY_FIELD_TYPES).get(
+                form.cleaned_data.get('geometry_field', DEFAULT_MD_GEOMETRY_FIELD_TYPE))
+            geometry_field = geometry_model(
+                name=DEFAULT_MD_GEOMETRY_FIELD_NAME, model_def=obj, null=True, blank=True)
+            geometry_field.save()
+
 
 admin.site.register(ModelDefinition, ModelDefinitionAdmin)
 
@@ -81,10 +103,13 @@ class ModelDefinitionAdminBuilder(object):
             registry[m] = a
         admin.site._registry = registry
         for o in self.get_models():
-            c = o.model_class()
-            cc = deepcopy(c)
-            cc._meta.app_label = 'objects'
-            admin.site.register(cc, self.get_admin_class(o))
+            try:
+                c = o.model_class()
+                cc = deepcopy(c)
+                cc._meta.app_label = 'objects'
+                admin.site.register(cc, self.get_admin_class(o))
+            except:
+                pass
 
     @classmethod
     def get_models(cls):
@@ -94,7 +119,9 @@ class ModelDefinitionAdminBuilder(object):
     def get_admin_class(cls, o):
         class Alass(cls.AdminClass):
             fields = [f.name for f in o.fielddefinitions.order_by('pk')]
+
         return Alass
+
 
 builder = ModelDefinitionAdminBuilder()
 builder.build()
