41b4318e51c122836c380b290f84404104f83b2e
Alexey Baranov <baranov@digitalcore.ru>
2015-09-18 17:50:31 +0300

    upd tableproxy

--- registrator/settings/__init__.py
+++ registrator/settings/__init__.py
@@ -156,7 +156,6 @@ USERLAYERS_MD_MODEL = 'main.MainModelDef'
 # USERLAYERS_MD_CLASS_RESERVED_NAMES = ['image', 'video']
 USERLAYERS_ADMIN_MD_CLASS = 'main.admin.MainModelDefinitionAdmin'
 USERLAYERS_ADMIN_MD_OBJECT_CLASS = 'main.admin.MainModelDefinitionObjectAdmin'
-USERLAYERS_API_TABLE_PROXY_CLASS = 'main.api.TableProxyResource'
 USERLAYERS_API_TABLE_EXCLUDE_FIELDS = ['user_id', 'user']
 
 try:
