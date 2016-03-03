447604f60a89a0fa5547cc4385fd131d3daaa3ed
Alexey Baranov <baranov@digitalcore.ru>
2016-03-03 08:53:32 +0300

    choice field repo save 3

--- userlayers_admin/admin/fields/foreign_key.py
+++ userlayers_admin/admin/fields/foreign_key.py
@@ -1,30 +1,25 @@
 # coding: utf-8
-from django.forms import ChoiceField
-from userlayers_admin.admin.fields.base import FieldAdmin, FieldFormAdmin, ModelDef
+from userlayers_admin.admin.fields.base import FieldAdmin, FieldFormAdmin
 
 
 class ForeignKeyFieldFormAdmin(FieldFormAdmin):
     def __init__(self, *args, **kwargs):
+        from userlayers_admin.admin import ModelDefinitionAdminBuilder
         super(ForeignKeyFieldFormAdmin, self).__init__(*args, **kwargs)
-        self.fields['to'] = ChoiceField(
-            [(md.pk, md.verbose_name) for md in
-             getattr(ModelDef, 'admin_objects_objects', ModelDef.objects).all()])
-
-    def clean(self):
-        cleaned_data = super(ForeignKeyFieldFormAdmin, self).clean()
-        if 'to' in cleaned_data:
-            cleaned_data['to'] = ModelDef.objects.get(pk=cleaned_data['to'])
-        return cleaned_data
+        self.fields['to'].choices = [(md.pk, md.verbose_name) for md in ModelDefinitionAdminBuilder.get_models()]
 
 
 class ForeignKeyFieldAdmin(FieldAdmin):
     form = ForeignKeyFieldFormAdmin
+
+    def get_queryset(self, request):
+        return super(ForeignKeyFieldAdmin, self).get_queryset(request).prefetch_related('to')
 
     def get_list_display(self, request):
         return list(super(ForeignKeyFieldAdmin, self).get_list_display(request)) + ['to_human_name']
 
     # TODO может кеш?
     def to_human_name(self, obj):
-        return ModelDef.objects.get(name=obj.to).verbose_name
+        return obj.to
 
     to_human_name.short_description = 'To'
