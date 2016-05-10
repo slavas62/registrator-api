70ba04a268790439a76c145b4a84b3fd0aea247e
Alexey Baranov <baranov@digitalcore.ru>
2016-05-11 02:42:21 +0300

    fix exif on save

--- main/__init__.py
+++ main/__init__.py
@@ -2,8 +2,8 @@
 from django.db.models import signals
 from django.contrib.auth import get_user_model
 from tastypie.models import create_api_key
-import main.signals_receivers
-import main.api
-import main.admin_user
 
 signals.post_save.connect(create_api_key, sender=get_user_model())
+
+import main.api
+import main.admin_user
